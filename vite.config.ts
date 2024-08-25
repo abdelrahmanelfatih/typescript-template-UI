78afb1b7fb8cc12f36902421a965dca5346ac790
humadi <a7.h3.04@gmail.com>
2024-08-25 11:28:24 +0800

    enable host

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   publicDir: './public',
   server: {
     // Uncomment this if you're running on a local network for testing:
-    // host: true,
+    host: true,
   },
 });
 
